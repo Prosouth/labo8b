1f2578bff8bac603dc867a3b211baa60aaf4614a
syl-b <syl-b@LAPTOP-A38F926I.einet.ad.eivd.ch>
2016-12-21 15:14:09 +0100

    Merge origin/master
    Conflicts:
    	main.cpp

--- main.cpp
+++ main.cpp
@@ -173,7 +173,7 @@ void testToutSupprimerValeur()
 
 void supprimerDoublons(int tab[], size_t& taille) 
 {
-   int tailleTabDoublon = taille / 2 + 1;
+   int tailleTabDoublon = taille / 2 ;
    bool doublon = true;
    int  nombreDoublon = 0;
    int tabDoublon[tailleTabDoublon];
@@ -184,19 +184,25 @@ void supprimerDoublons(int tab[], size_t& taille)
          if(tab[i] == tabDoublon[j])
          {
             doublon = false;
-            for(int h = i; h < taille-1; h++)
+            for(int h = i; h < taille; h++)
             {
+               if(tab[h] == tab[h+1])
+               {
+                  i--;
+                  //taille--;
+               }
                tab[h] = tab[h+1];
+   
             }
             taille--;
-          
+            
          }
       }
       if(doublon)
       {
          tabDoublon[nombreDoublon] = tab[i];
          doublon = true;
-         nombreDoublon ++;
+         nombreDoublon++;
       }
    }
 }
